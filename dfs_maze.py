9ed3e57a9fea56d7c91453f18518ea338d06f833
PatricioIribarneCatella <patricio_14_94@hotmail.com>
2019-05-02 20:17:28 -0300

    remove unnecessary

--- dfs_maze.py
+++ dfs_maze.py
@@ -4,9 +4,7 @@ from graph import MazeGraph
 
 class DFSMaze(MazeGraph):
 
-    def _dfs_circuit(self, position, visited, res):
-
-        res.append(position)
+    def _dfs_circuit(self, position, visited):
 
         visited[position] = True
 
@@ -17,17 +15,17 @@ class DFSMaze(MazeGraph):
         for t in neighbours:
             if not visited[t]:
                 self._removeWall(position, t)
-                self._dfs_circuit(t, visited, res)
+                self._dfs_circuit(t, visited)
 
     def create(self):
 
         visited = {(i, j):False for j in range(self.width) for i in range(self.height)}
 
-        res = []
         self.adjacency_lists = [[[] for j in range(self.width)] for i in range(self.height)] # Nothing is accesible at first
 
         # Obtains the DFS circuit (tree)
         # while removing the edges (walls)
         # that connects two cells
-        self._dfs_circuit((0, 0), visited, res)
+        #
+        self._dfs_circuit((0, 0), visited)
 
