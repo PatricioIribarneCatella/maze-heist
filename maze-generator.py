ed28ef85dc611afd45ad822bbb5f5ecb70c1e6ec
gonzalpetraglia <gonzalpetraglia@gmail.com>
2019-05-06 02:11:10 -0300

    Revert "Plot maze"

--- maze-generator.py
+++ maze-generator.py
@@ -4,12 +4,10 @@ import sys
 
 from dfs_maze import DFSMaze
 from dac_maze import DACMaze
-from maze_plot import plot
 
 FILE_OUTPUT = "mapa-laberinto.txt"
 DFS = "dfs"
 DAC = "dyc"
-
 
 def write_maze(maze):
 
@@ -24,10 +22,8 @@ def main(method, height, width):
         maze = DACMaze(width, height)
 
     maze.create()
-    
-    mazeStr = maze.toString()
-    write_maze(mazeStr)
-    plot(mazeStr)
+
+    write_maze(maze.toString())
 
 def parse_input(params):
 
