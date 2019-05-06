3087c41e88bc559120c6ed77904f5f22f2d94f95
PatricioIribarneCatella <patricio_14_94@hotmail.com>
2019-05-06 12:55:05 -0300

    Infer the correct Python version installed on the system

--- Parte-1/tester.sh
+++ Parte-1/tester.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
+
+PYVER=$(python3 --version | sed 's/^.* //g' | cut -f2 -d.)
+
 for i in {0..100}
  do 
-     python3.7 maze-generator.py dyc 30 30
-     python3.7 maze-solver.py
+     python3.$PYVER maze-generator.py dyc 30 30
+     python3.$PYVER maze-solver.py
  done
 
 
 for i in {0..100}
  do 
-     python3.7 maze-generator.py dfs 30 30
-     python3.7 maze-solver.py
+     python3.$PYVER maze-generator.py dfs 30 30
+     python3.$PYVER maze-solver.py
  done
 
 
